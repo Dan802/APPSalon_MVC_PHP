ce9f0d1440e8af2e9151a82cb25d21929fe1c33c
Dan802 <jfelipeg05@gmail.com>
2023-10-10 12:42:58 -0500

    add description

--- views/layout.php
+++ views/layout.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Sitio web para el agendamiento de citas con estilistas en Colombia">
     <title>App Salón</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="/build/css/app.css">
