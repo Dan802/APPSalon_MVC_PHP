69f579cf658499bb4dea39297ad72c93ab9bb0ab
Dan802 <jfelipeg05@gmail.com>
2024-01-23 22:29:09 -0500

    Finalizado

--- clases/Email.php
+++ clases/Email.php
@@ -20,15 +20,18 @@ class Email {
         // Crear el objeto de email
         $phpmailer = new PHPMailer();
         $phpmailer->isSMTP();
-        $phpmailer->Host = $_ENV['EMAIL_HOST'];
         $phpmailer->SMTPAuth = true;
+
+        $phpmailer->Host = $_ENV['EMAIL_HOST'];
         $phpmailer->Port = $_ENV['EMAIL_PORT'];
         $phpmailer->Username = $_ENV['EMAIL_USER'];
         $phpmailer->Password = $_ENV['EMAIL_PASS'];
 
-        $phpmailer->setFrom('cuentas@appsalon.com');
-        $phpmailer->addAddress('cuentas@appsalon.com', 'AppSalon.com');
-        $phpmailer->Subject = 'Confrima tu cuenta';
+        // $phpmailer->setFrom('cuentas@appsalon.com');
+        $phpmailer->setFrom($_ENV['EMAIL_USER'], 'Cita Perruna');
+        // $phpmailer->addAddress('cuentas@appsalon.com', 'AppSalon.com');
+        $phpmailer->addAddress($this->email, $this->nombre);
+        $phpmailer->Subject = 'Confirma tu Cuenta';
 
         // Set HTML
         $phpmailer->isHTML(TRUE);
@@ -36,28 +39,40 @@ class Email {
 
         $contenido = "<html>";
         $contenido .= "<p><strong>¡Hola! ¿Que tal ". $this->nombre . "?</strong>";
-        $contenido .= "<br>Has creado tu cuenta exitosamente en App Salon, solo debes confirmarla presionando el siguiente enlace.</p>";
+        $contenido .= "<br>Has creado tu cuenta exitosamente en Cita Perruna, para activar tu cuenta solo da clic en el enlace, ¡Es gratis!.</p>";
         $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['PROJECT_URL'] . "/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";
         $contenido .= "<p>Si no has solicitado la creación de esta cuenta, puedes ignorar el mensaje.</p>";
         $contenido .= "</html>";
         
         $phpmailer->Body = $contenido;
 
-        $phpmailer->send();    // Enviar el email
+        $phpmailer->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $phpmailer->AltBody = "Ve al siguiente enlace para activar tu cuenta:  " . $_ENV['HOST'] . "/confirmar-cuenta?token=" . $this->token;
+
+        if($phpmailer->send()){
+            echo 'Message has been sent';
+            return true;
+        }else{
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $phpmailer->ErrorInfo;
+            return false;
+        }
+        // $phpmailer->send();    // Enviar el email
     }
 
     public function enviarInstrucciones() {
         // Crear el objeto de email
         $phpmailer = new PHPMailer();
         $phpmailer->isSMTP();
-        $phpmailer->Host = $_ENV['EMAIL_HOST'];
         $phpmailer->SMTPAuth = true;
+
+        $phpmailer->Host = $_ENV['EMAIL_HOST'];
         $phpmailer->Port = $_ENV['EMAIL_PORT'];
         $phpmailer->Username = $_ENV['EMAIL_USER'];
         $phpmailer->Password = $_ENV['EMAIL_PASS'];
 
-        $phpmailer->setFrom('cuentas@appsalon.com');
-        $phpmailer->addAddress('cuentas@appsalon.com', 'AppSalon.com');
+        $phpmailer->setFrom($_ENV['EMAIL_USER'], 'Cita Perruna');
+        $phpmailer->addAddress($this->email, $this->nombre);
         $phpmailer->Subject = 'Reestablecer tu Contraseña';
 
         // Set HTML
@@ -68,11 +83,21 @@ class Email {
         $contenido .= "<p><strong>¡Hola! ¿Que tal ". $this->nombre . "?</strong>";
         $contenido .= "<br>Has solicitado reestablecer tu contraseña, sigue el siguiente enlace para hacerlo</p>";
         $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['PROJECT_URL'] . "/recuperar?token=" . $this->token ."'>Reestablecer Contraseña</a>";
-        $contenido .= "<p>Si no has solicitado reestablecer tu contraseña, puedes ignorar el mensaje.</p>";
+        $contenido .= "<p>Si no has solicitado reestablecer tu contraseña, puedes ignorar este mensaje.</p>";
         $contenido .= "</html>";
         
         $phpmailer->Body = $contenido;
 
-        $resultado = $phpmailer->send();    // Enviar el email
+        $phpmailer->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $phpmailer->AltBody = "Ve al siguiente enlace para reestablecer tu constraseña:  " . $_ENV['HOST'] . "/recuperar?token=" . $this->token;
+
+        if($phpmailer->send()){
+            echo 'Message has been sent';
+            return true;
+        }else{
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $phpmailer->ErrorInfo;
+            return false;
+        }
     }
 }
